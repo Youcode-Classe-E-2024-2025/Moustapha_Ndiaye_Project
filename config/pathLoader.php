b094de68fc66f64248487fc33036b1584ca259ac
amdymstfa <amdymoustapha011@gmail.com>
2025-01-07 15:24:30 +0100

    done : view page of manager

---
+++ config/pathLoader.php
@@ -0,0 +1,18 @@
+<?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    // import require file
+    require_once('config.php') ;
+    require_once('../config/loadDatabase.php');
+    require_once('../src/controllers/authentificationController.php');
+
+    // create database
+    $db = new database() ;
+    $pdo = $db->connexion();
+
+    // load script database 
+    $loader = new LoadDatabase($pdo, '../database/schemaDatabase.sql');
+    $loader->fetchData();
+    
+?>
