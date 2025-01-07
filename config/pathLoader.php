87b048fad3ec2f581b972a5ab020785b1821c495
amdymstfa <amdymoustapha011@gmail.com>
2025-01-07 16:25:43 +0100

    done : remove mvc of manager

--- config/pathLoader.php
+++ config/pathLoader.php
@@ -3,7 +3,7 @@
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
     // import require file
-    require_once('config.php') ;
+    require_once('../config/config.php') ;
     require_once('../config/loadDatabase.php');
     require_once('../src/controllers/authentificationController.php');
 
